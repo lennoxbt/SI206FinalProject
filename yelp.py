afd13f92db9a51c503398ddfdd6f5cb17ad469f3
lennoxbt <91291351+lennoxbt@users.noreply.github.com>
2022-04-25 12:32:34 -0400

    Update yelp.py

--- yelp.py
+++ yelp.py
@@ -7,18 +7,14 @@ import time
 
 #Business Search      URL -- 'https://api.yelp.com/v3/businesses/search'
 
-# Define a business ID
-# business_id = '4AErMBEoNzbk7Q8g45kKaQ'
-
-def getYelpRatings():
-    # This function gathers data from the yelp api, 20 items at a time. It accesses the restaurant names in the restaurant.csv file in order to 
-    # pull multiple restaurant metrics from the api. This pulled data is then placed into the tables of the restaurantData.db 
+def GetYelpRatings():
+    # This function gathers data from the yelp api, 20 items at a time. It accesses the movie titles in the movie.csv file in order to 
+    # pull multiple movie metrics from the api. This pulled data is then places into the tables of the movieData.db 
     # database.
 
-    api_key = 'HfqofispM8DGunRZH5-sweTfY7YZQ3dppTieqg1FyH9rSi8fpaYDMhcVOQuLkYOfQvYwZPGzqkkkb1iQz2I5nmR1Pj7ApLHxrDZJ0nFJEyc7Cg8N25ngbdypujZPYnYx'
+    api_key = 'MG1W3tSf9JH2M0wgcH8WF57_7OW8wGBb6JjTFUxQ4YoXdvZkpmuBjNcydtePtFZISQlpXxnktL7DAiBKaOi2zwv9EwXC-0xb9FG9HeHGLTaBwnnV-75rN9qU7FtmYnYx'
     ENDPOINT = 'https://api.yelp.com/v3/businesses/search'
     rest_rows = []
-
     with open('restaurant.csv', 'r') as file:
         reader = csv.reader(file)
         count = 0
@@ -26,15 +22,14 @@ def getYelpRatings():
         conn = sqlite3.connect(dbName)
         cursor = conn.cursor()
         next(reader)
-
         for row in reader:
             count += 1
             #print(row)
             restaurantName = row[0]
-            # restaurantType = row[1]
-            location = row[2]
+            restaurantType = row[1]
             id = 0
 
+            location = row[2]
             parameters = {'term': restaurantName, 'location': location, 'categories': "restaurants", 'limit': 1}
             HEADERS = {'Authorization': 'bearer %s' % api_key}
             response_obj = requests.get(url = ENDPOINT, params = parameters, headers = HEADERS)
@@ -42,23 +37,29 @@ def getYelpRatings():
             #print(data['businesses'][0])
             #print(data.keys())
             #print(type(data.keys()))
-
+           # print(data)
             if 'businesses' in data.keys():
                 #print(json.dumps(data, indent = 3))
                 if data['businesses'] != []:
-                    Yelp_Rating = (data['businesses'][0]['rating'])
+                    yelp_rating = (data['businesses'][0]['rating'])  
                 else:
-                    Yelp_Rating = 0
-            #print(Yelp_Rating)
+                    yelp_rating = 0
+                #print(yelp_rating)
+            cursor.execute("UPDATE Restaurants SET Yelp_rating = ? WHERE Name = ?", (yelp_rating, restaurantName))
+            conn.commit()
+            #cursor.execute('SELECT * FROM Restaurants JOIN Types ON Restaurants.Type_id = Types.type WHERE Type_id = ?', ("BLOOP"))
+        
 
-            cursor.execute("SELECT * FROM Types")
-            for row in cursor:
-                #print(row)
-                if type == row[1]:
-                    id = row[0]
-            
-            cursor.execute("UPDATE Restaurants SET Type_ID = ? WHERE Name = ?", (id, restaurantName))
-            cursor.execute("UPDATE Restaurants SET Yelp_Rating = ? WHERE Name = ?", (id, restaurantName))
+
+        cursor.execute("SELECT * FROM Restaurants")
+        for row in cursor:
+            id = row[0]
+            print(row)
+
+        # cursor.execute("UPDATE Restaurants SET Type_id = ?", ('HANNNN')) 
+        # cursor.execute("SELECT * FROM Restaurants")
+        # for row in cursor:
+        #     print(row)         
 
 def yelp_csv(filename):
     # This function takes in a csv filename as input, and selects data from the joined tables, Restaurants and Types. 
@@ -94,6 +95,9 @@ def yelp_csv(filename):
 def main():
     # This function calls the above functions, getYelpRatings and yelp_csv. In order to gather enough data from 
     # the Yelp API, getYelpRatings is called within a for loop, multiple times.
+            
+    GetYelpRatings()
+    print('done')
 
     # for i in range(1,11):
     #     time.sleep(1)
