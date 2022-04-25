7eb725856a5a68dee2847a08be64ea3c78a34844
lennoxbt <91291351+lennoxbt@users.noreply.github.com>
2022-04-24 23:59:07 -0400

    Update opentable.py

--- opentable.py
+++ opentable.py
@@ -16,6 +16,7 @@ def restaurantlst(url):
     restaurant_names =[]
     restaurant_location =[]
     restaurant_Type = []
+    restaurant_Rating = []
     response = requests.get(url)
     soup = BeautifulSoup(response.content,'html.parser')
 
@@ -25,15 +26,26 @@ def restaurantlst(url):
       restaurantName = item.find('h2', class_='H5kqSXUFObkmV6wfAw7p').text
       restaurantLocation =  item.find('div', class_='GwGPDuPdTZpMt7oIzo4A YQ2SmR3lcmrtTcxuktZA').text
       restaurantType =  item.find('div', class_= 'UNOIq8wcqctrC7s3wHAW YQ2SmR3lcmrtTcxuktZA').text
+      restaurantRating = item.find('div', class_ = 'vJWFYZLiWZbHIB0Hwa83')['aria-label'][:3]
+      if restaurantRating[0] == 5:
+        print("hooray")
+        #restaurantRating == 5.0
+      # else:
+      #   restaurantRating = float(restaurantRating)
+      
+
       restaurant_names.append(restaurantName)
       restaurant_Type.append(restaurantType)
       restaurant_location.append(restaurantLocation)
+      restaurant_Rating.append(restaurantRating)
+
+
 
     #print(restaurant_location)
     #print(restaurant_names)
     #print(list(zip(restaurant_names, restaurant_location)))
-    restaurantlst = list(zip(restaurant_names, restaurant_Type, restaurant_location))
-    print(restaurantlst)
+    restaurantlst = list(zip(restaurant_names, restaurant_Type, restaurant_location, restaurant_Rating))
+    #print(restaurantlst)
     return restaurantlst
 
 
@@ -48,35 +60,39 @@ def setUpDatabase(db_name):
    return cur, conn
   
 
-  #Create Restaurants table if not exists
+  #Create restaurant table if not exists 
+
 def setUpRestaurantTable(cur, conn, restaurantlst, x = 0):
   # This takes a database cursor and conneciton, list of restaurant names, restaurant type, and optional 
   # argument which specifies the starting position of the database id. The function creates a table, Restaurants, 
   # within the passed database and inserts each restaurant in restaurantlist, along with its corresponding id, location, and type.
-  cur.execute("CREATE TABLE IF NOT EXISTS Restaurants (id INTEGER PRIMARY KEY, Name TEXT, Type TEXT, Location TEXT, OpenTable_Rating FLOAT, Yelp_Rating FLOAT)")
+  cur.execute("CREATE TABLE IF NOT EXISTS Restaurants (id INTEGER PRIMARY KEY,Name TEXT,Type TEXT,Type_id TEXT,Location TEXT, opentable_rating FLOAT, Yelp_rating INTEGER, YELP2 FLOAT, YELP3 FLOAT, YELP4 FLOAT)")
   for num in range(len(restaurantlst)):
     id = num + x
-    cur.execute("INSERT INTO Restaurants (id,Name,Type,Location) VALUES (?,?,?,?)",(id,restaurantlst[num][0],restaurantlst[num][1],restaurantlst[num][2]))
+    cur.execute("INSERT INTO Restaurants (id,Name,Type,Location,opentable_rating) VALUES (?,?,?,?,?)",(id,restaurantlst[num][0],restaurantlst[num][1],restaurantlst[num][2],restaurantlst[num][3][:3]))
   conn.commit()
 
   
   # Create Type table is not exists
+
 def setUpTypeTable(cur, conn, typelst):
     # This function takes in a databse cursor and connection, as well as a list of restaurant type. It then creates
     # a table, Types, within the database, along with its corresponding id number.
 
-    # I FIXED THIS FUNCTION
     cur.execute("CREATE TABLE IF NOT EXISTS Types (id INTEGER PRIMARY KEY, type TEXT)")
     for i in range(len(typelst)):
         cur.execute("INSERT OR IGNORE INTO Types (id, type) VALUES (?,?)", (i, typelst[i]))
     conn.commit()
+
+
+
 
 # Write restuaurant list into a CSV file
 def write_csv(data, filename):
   # This file takes in a list of tuples and csv filename as input. It then iterates through the list of tuples 
   # to write multiple rows within the csv, outputting a csv file containing each restaurant name and restaurant location.
   
-  header = ('Restaurant Name', 'Restaurant Location') #, 'Rating')
+  header = ('Restaurant Name', 'Restaurant Type', 'Restaurant Location', 'Restaurant Rating')
 
   with open(filename, 'w', newline='') as f:
          writer = csv.writer(f)
@@ -84,20 +100,24 @@ def write_csv(data, filename):
          for item in data:
              writer.writerow(item)
  
-restaurantlst('https://www.opentable.com/lists/top-100-2021')
+
+  # Setup database file
+
+
+
 
 def main():
-  # This function calls all the above functions, setting up the database, defining the restaurant types, 
-  # grabbing the lists of restaurants, writing the csv, and setting up the database tables.
 
   restaurant_tuple_lst = restaurantlst('https://www.opentable.com/lists/top-100-2021')
-  # I ADDED THE TYPES OF RESTAURANTS HERE
   types = ['Afghan', 'American', 'Contemporary American', 'Contemporary French', 'Contemporary Southern', 'Croatian', 'Farm-to-table', 'Fish', 'French', 'Fusion / Eclectic', 'Greek', 'Italian', 'Mediterranean', 'Mexican', 'Peruvian', 'Seafood', 'Southwest', 'Speakeasy', 'Steak', 'Steakhouse', 'Sushi', 'Tapas / Small Plates', 'Traditional French', 'Vietnamese', 'Winery']
+  
+  write_csv(restaurant_tuple_lst, 'restaurant.csv')
 
+  
   cur, conn = setUpDatabase('restaurantData.db')
-
-  # TYPES TABLE ADDED
-  setUpTypeTable(cur, conn, types)
   setUpRestaurantTable(cur, conn, restaurant_tuple_lst, x = 0)
+  setUpTypeTable(cur, conn, types)
+
+
 
 main()
