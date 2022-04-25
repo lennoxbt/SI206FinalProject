4a82c3b9d3f67ef0dd65b1f3eff66fe60648955a
lennoxbt <91291351+lennoxbt@users.noreply.github.com>
2022-04-25 00:00:01 -0400

    Create yelp.py

---
+++ yelp.py
@@ -0,0 +1,67 @@
+import requests
+import re
+import csv
+import json
+import sqlite3
+import time
+
+#Business Search      URL -- 'https://api.yelp.com/v3/businesses/search'
+
+
+# # Define a business ID
+# business_id = '4AErMBEoNzbk7Q8g45kKaQ'
+
+def GetYelpRatings():
+    # This function gathers data from the yelp api, 20 items at a time. It accesses the movie titles in the movie.csv file in order to 
+    # pull multiple movie metrics from the api. This pulled data is then places into the tables of the movieData.db 
+    # database.
+
+    api_key = 'HfqofispM8DGunRZH5-sweTfY7YZQ3dppTieqg1FyH9rSi8fpaYDMhcVOQuLkYOfQvYwZPGzqkkkb1iQz2I5nmR1Pj7ApLHxrDZJ0nFJEyc7Cg8N25ngbdypujZPYnYx'
+    ENDPOINT = 'https://api.yelp.com/v3/businesses/search'
+    rest_rows = []
+    with open('restaurant.csv', 'r') as file:
+        reader = csv.reader(file)
+        count = 0
+        dbName ='restaurantData.db'
+        conn = sqlite3.connect(dbName)
+        cursor = conn.cursor()
+        next(reader)
+        for row in reader:
+            count += 1
+            #print(row)
+            restaurantName = row[0]
+            restaurantType = row[1]
+            id = 0
+
+            location = row[2]
+            parameters = {'term': restaurantName, 'location': location, 'categories': "restaurants", 'limit': 1}
+            HEADERS = {'Authorization': 'bearer %s' % api_key}
+            response_obj = requests.get(url = ENDPOINT, params = parameters, headers = HEADERS)
+            data = response_obj.json()
+            #print(data['businesses'][0])
+            #print(data.keys())
+            #print(type(data.keys()))
+            if 'businesses' in data.keys():
+                #print(json.dumps(data, indent = 3))
+                if data['businesses'] != []:
+                    yelp_rating = (data['businesses'][0]['rating'])
+                else:
+                    yelp_rating = 0
+               
+            #print(yelp_rating)
+            
+            
+            cursor.execute("SELECT * FROM Types")
+            for row in cursor:
+                #print(row)
+                if type == row[1]:
+                    id = row[0]
+            
+            cursor.execute("UPDATE Restaurants SET Type_id = ? WHERE Name = ?", (id, restaurantName))
+
+            cursor.execute("UPDATE Restaurants SET Yelp_rating = ? WHERE Name = ?", (yelp_rating, restaurantName))
+
+            
+
+
+GetYelpRatings()
