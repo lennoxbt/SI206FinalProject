12508a3ff076ddc20c64fc6cd33763bae65d1a2e
lennoxbt <91291351+lennoxbt@users.noreply.github.com>
2022-04-25 12:31:17 -0400

    Update opentable.py

--- opentable.py
+++ opentable.py
@@ -13,8 +13,8 @@ def restaurantlst(url):
   # This function takes in a url from the "Open Table" website, and creates a BeautifulSoup Object to parse through 
   # the site's HTML. The function returns a list of tuples which include the restaurant name as well as the restaurant's location.
     
-    restaurant_Name = []
-    restaurant_Location = []
+    restaurant_names =[]
+    restaurant_location =[]
     restaurant_Type = []
     restaurant_Rating = []
     response = requests.get(url)
@@ -27,23 +27,29 @@ def restaurantlst(url):
       restaurantLocation =  item.find('div', class_='GwGPDuPdTZpMt7oIzo4A YQ2SmR3lcmrtTcxuktZA').text
       restaurantType =  item.find('div', class_= 'UNOIq8wcqctrC7s3wHAW YQ2SmR3lcmrtTcxuktZA').text
       restaurantRating = item.find('div', class_ = 'vJWFYZLiWZbHIB0Hwa83')['aria-label'][:3]
-      if restaurantRating[0] == 5:
-        print("hooray")
-        #restaurantRating == 5.0
-      # else:
-      #   restaurantRating = float(restaurantRating)
+      if restaurantRating == '5 s':
+        restaurantRating = 5.0
+      else:
+        restaurantRating = float(restaurantRating)
+      print(restaurantRating)
+      print(type(restaurantRating))
+  
       
-      restaurant_Name.append(restaurantName)
+
+      restaurant_names.append(restaurantName)
       restaurant_Type.append(restaurantType)
-      restaurant_Location.append(restaurantLocation)
+      restaurant_location.append(restaurantLocation)
       restaurant_Rating.append(restaurantRating)
-    
+
+
+
     #print(restaurant_location)
     #print(restaurant_names)
     #print(list(zip(restaurant_names, restaurant_location)))
-    restaurantlst = list(zip(restaurant_Name, restaurant_Type, restaurant_Location, restaurant_Rating))
+    restaurantlst = list(zip(restaurant_names, restaurant_Type, restaurant_location, restaurant_Rating))
     #print(restaurantlst)
     return restaurantlst
+
 
 def setUpDatabase(db_name):
   # This function simply takes in a string as input which contains the preferred database file name, and 
@@ -54,43 +60,54 @@ def setUpDatabase(db_name):
    cur = conn.cursor()
 
    return cur, conn
+  
 
-# Create Restaurants table if does not exist
-def setUpRestaurantsTable(cur, conn, restaurantlst, x = 0):
+  #Create restaurant table if not exists 
+
+def setUpRestaurantTable(cur, conn, restaurantlst, x = 0):
   # This takes a database cursor and conneciton, list of restaurant names, restaurant type, and optional 
   # argument which specifies the starting position of the database id. The function creates a table, Restaurants, 
   # within the passed database and inserts each restaurant in restaurantlist, along with its corresponding id, location, and type.
-  
-  cur.execute("CREATE TABLE IF NOT EXISTS Restaurants (id INTEGER PRIMARY KEY, Name TEXT, Type TEXT, Type_ID TEXT, Location TEXT, OpenTable_Rating FLOAT, Yelp_Rating FLOAT)")
+  cur.execute("CREATE TABLE IF NOT EXISTS Restaurants (id INTEGER PRIMARY KEY,Name TEXT,Type TEXT,Type_id TEXT,Location TEXT, opentable_rating FLOAT, Yelp_rating INTEGER, YELP2 FLOAT, YELP3 FLOAT, YELP4 FLOAT)")
   for num in range(len(restaurantlst)):
     id = num + x
-    cur.execute("INSERT OR IGNORE INTO Restaurants (id, Name, Type, Location, OpenTable_Rating) VALUES (?,?,?,?,?)", (id, restaurantlst[num][0], restaurantlst[num][1], restaurantlst[num][2], restaurantlst[num][3][:3]))
+    cur.execute("INSERT INTO Restaurants (id,Name,Type,Location,opentable_rating) VALUES (?,?,?,?,?)",(id,restaurantlst[num][0],restaurantlst[num][1],restaurantlst[num][2],restaurantlst[num][3]))
   conn.commit()
 
-# Create Types table is not exists
-def setUpTypesTable(cur, conn, typelst):
+  
+  # Create Type table is not exists
+
+def setUpTypeTable(cur, conn, typelst):
     # This function takes in a databse cursor and connection, as well as a list of restaurant type. It then creates
     # a table, Types, within the database, along with its corresponding id number.
 
-    cur.execute("CREATE TABLE IF NOT EXISTS Types (id INTEGER PRIMARY KEY, type TEXT)")
+    cur.execute("CREATE TABLE IF NOT EXISTS Types (id INTEGER PRIMARY KEY, Type_id TEXT)")
     for i in range(len(typelst)):
-        cur.execute("INSERT OR IGNORE INTO Types (id, type) VALUES (?,?)", (i, typelst[i]))
+        cur.execute("INSERT OR IGNORE INTO Types (id, Type_id) VALUES (?,?)", (i, typelst[i]))
     conn.commit()
 
-# Write restaurant list into a CSV file
+
+
+
+# Write restuaurant list into a CSV file
 def write_csv(data, filename):
   # This file takes in a list of tuples and csv filename as input. It then iterates through the list of tuples 
   # to write multiple rows within the csv, outputting a csv file containing each restaurant name and restaurant location.
   
-  header = ('Restaurant Name', 'Restaurant Type', 'Restaurant Location', 'Restaurant OpenHouse Rating')
+  header = ('Restaurant Name', 'Restaurant Type', 'Restaurant Location', 'Restaurant Rating')
 
   with open(filename, 'w', newline='') as f:
          writer = csv.writer(f)
          writer.writerow(header)
          for item in data:
              writer.writerow(item)
-  
-# Setup database file
+ 
+
+  # Setup database file
+
+
+
+
 def main():
 
   restaurant_tuple_lst = restaurantlst('https://www.opentable.com/lists/top-100-2021')
@@ -98,8 +115,12 @@ def main():
   
   write_csv(restaurant_tuple_lst, 'restaurant.csv')
 
+  
   cur, conn = setUpDatabase('restaurantData.db')
-  setUpRestaurantsTable(cur, conn, restaurant_tuple_lst, x = 0)
-  setUpTypesTable(cur, conn, types)
+  setUpRestaurantTable(cur, conn, restaurant_tuple_lst, x = 0)
+  setUpTypeTable(cur, conn, types)
+
+
 
 main()
+
