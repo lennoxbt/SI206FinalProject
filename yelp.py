ca3cf97a6e24b5d154ef8545e29f4d745fae13d6
nsurcel <98379717+nsurcel@users.noreply.github.com>
2022-04-26 03:52:28 -0400

    Update yelp.py

--- yelp.py
+++ yelp.py
@@ -4,17 +4,24 @@ import csv
 import json
 import sqlite3
 import time
+import opentable
+import visualizations
+import matplotlib.pyplot as plt
 
 #Business Search      URL -- 'https://api.yelp.com/v3/businesses/search'
 
-def GetYelpRatings():
-    # This function gathers data from the yelp api, 20 items at a time. It accesses the movie titles in the movie.csv file in order to 
-    # pull multiple movie metrics from the api. This pulled data is then places into the tables of the movieData.db 
+# Define a business ID
+# business_id = '4AErMBEoNzbk7Q8g45kKaQ'
+
+def getYelpRatings():
+    # This function gathers data from the Yelp API, 20 items at a time. It accesses the restaurant names in the restaurant.csv file in order to 
+    # pull multiple restaurant metrics from the api. This pulled data is then placed into the tables of the restaurantData.db 
     # database.
 
     api_key = 'MG1W3tSf9JH2M0wgcH8WF57_7OW8wGBb6JjTFUxQ4YoXdvZkpmuBjNcydtePtFZISQlpXxnktL7DAiBKaOi2zwv9EwXC-0xb9FG9HeHGLTaBwnnV-75rN9qU7FtmYnYx'
     ENDPOINT = 'https://api.yelp.com/v3/businesses/search'
     rest_rows = []
+
     with open('restaurant.csv', 'r') as file:
         reader = csv.reader(file)
         count = 0
@@ -22,54 +29,99 @@ def GetYelpRatings():
         conn = sqlite3.connect(dbName)
         cursor = conn.cursor()
         next(reader)
+
         for row in reader:
-            #count += 1
+            # count += 1
             print(row)
             restaurantName = row[0]
             restaurantType = row[1]
+            restaurantLocation = row[2]
+            # restaurantYelpRating = row[3]
             id = 0
 
-            location = row[2]
-            parameters = {'term': restaurantName, 'location': location, 'categories': "restaurants", 'limit': 1}
+            parameters = {'term': restaurantName, 'location': restaurantLocation, 'categories': 'restaurants', 'limit': 1}
             HEADERS = {'Authorization': 'bearer %s' % api_key}
             response_obj = requests.get(url = ENDPOINT, params = parameters, headers = HEADERS)
             data = response_obj.json()
             #print(data['businesses'][0])
             #print(data.keys())
             #print(type(data.keys()))
-           # print(data)
+
             if 'businesses' in data.keys():
                 #print(json.dumps(data, indent = 3))
                 if data['businesses'] != []:
-                    yelp_rating = (data['businesses'][0]['rating'])  
+                    # print(json.dumps(data['businesses'][0]['price'], indent = 3))
+                    yelp_Rating = (data['businesses'][0]['rating'])
+                    if 'price' in data['businesses'][0]:
+                        yelp_Price = data['businesses'][0]['price']
+                    else:
+                        yelp_Price = 'N/A'
                 else:
-                    yelp_rating = 0
-                #print(yelp_rating)
-            cursor.execute("UPDATE Restaurants SET Yelp_rating = ? WHERE Name = ?", (yelp_rating, restaurantName))
-            conn.commit()
-            cursor.execute('UPDATE Restaurants SET type_id=(select type_id from Types WHERE Types.type=Restaurants.type)')
+                    yelp_Rating = 0
+
+                # print(yelp_Rating)
+            
+            cursor.execute('UPDATE Restaurants SET Type_ID=(select Type_ID from Types WHERE Types.Type=Restaurants.Type)')
             #'UPDATE Restaurants SET type_id=(select type_id from Types WHERE Types.type=Restaurants.type)'
             conn.commit()
-        
+            cursor.execute("UPDATE Restaurants SET Yelp_Rating = ? WHERE Name = ?", (yelp_Rating, restaurantName))
+            conn.commit()
+            cursor.execute("UPDATE Restaurants SET Yelp_Price = ? WHERE Name = ?", (yelp_Price, restaurantName))
+            conn.commit()
 
-# get a cursor for "select type, typeid from types"
-# for each row in the cursor
-#     get type and type_id from the row
-#     update restaurant set type_id = (type_id)
-#     where restaurant.type =  (type)
+            # cursor.execute("SELECT * FROM Restaurants")
+            # for row in cursor:
+            #     id = row[0]
+            #     #print(row)
 
+            # if rest_rows:
+            #     with open('restaurant.csv', 'w') as file:    
+            #         writer = csv.writer(file)
+            #         writer.writerows(rest_rows)
 
-        # cursor.execute("SELECT Type_id  FROM Types")
-        # for row in cursor:
-        #     id = row[0]
-        #     print(row)
+def yelp_csv(filename):
+    # This function takes in a csv filename as input, and selects data from the joined tables, Restaurants and Types. 
+    # This data is then used to calculate the average of the OpenTable and Yelp ratings for each restaurant.
+    # This calculation is then output into the yelp.csv file along with the restaurant name, average rating btwn yelp and open table
 
-        # cursor.execute("SELECT * FROM Restaurants")
-        # for row in cursor:
-        #     print(row)         
+    header = ('Restaurant Name', 'Restaurant Average Rating')
 
-# UPDATE Restaurants
-# SET Type_id = (SELECT b.Type_id  FROM Restaurants a left JOIN Types b on a.Type = b.Type)
-        
-GetYelpRatings()
-print('done')
+    dbName ='restaurantData.db'
+    conn = sqlite3.connect(dbName)
+    cursor = conn.cursor()
+    
+    # cursor.execute('SELECT * FROM Restaurants JOIN Types ON Restaurants.Type_ID = Types.Type_ID WHERE OpenTable_Rating != ? AND Yelp_Rating != ?', (0,0))
+    with open(filename, 'w', newline ='') as file:
+        writer = csv.writer(file)
+        writer.writerow(header)
+
+        for row in cursor:
+            opentable_rating = row[5]
+            yelp_rating = row[6]
+            # print(row)
+            # print(opentable_rating,yelp_rating)
+            score = (float(opentable_rating) + float(yelp_rating))
+            avg = score/2
+            ro = [row[1], avg]
+            print(ro)
+            writer.writerow(ro)
+
+# def type_rating_vis(conn):
+#     type_rating_data = opentable.getTypeRatingData(conn)
+#     visualizations.Type_Rating_Vis(type_rating_data)
+#     plt.show()
+
+def main():
+    # This function calls the above functions, getYelpRatings and yelp_csv. In order to gather enough data from 
+    # the Yelp API, getYelpRatings is called within a for loop, multiple times.
+    
+    # for i in range(1,11):
+    #     time.sleep(1)
+    #     getYelpRatings()
+    #     hm = (i*20)
+    #     print(str(hm) + ' Items Collected')
+
+    getYelpRatings()  
+    yelp_csv('yelp.csv')
+
+main()
